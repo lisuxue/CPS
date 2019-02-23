12b21f690104e120fb2386cc7fab9f13e3ad5d6b
Suxue <suxueli@macbook-pro-de-suxue.home>
2019-02-23 17:50:24 +0100

    TME2 revoir diag composition et faire dernier exo

---
+++ TME2/maleva/src/maleva/agent/RandomMouvement.java
@@ -0,0 +1,46 @@
+
+package maleva.agent;
+
+import java.util.Random;
+
+import maleva.framework.LifeCycleException;
+
+public class RandomMouvement extends ComportementAgent {
+	
+	/* fields */
+	private Random rand;
+	
+	/* properties */
+	private int angle_step = 20;
+	
+	public RandomMouvement(Agent outer) {
+		super(outer);
+		rand = new Random();
+	}
+	
+	/* LCStepperController */
+	
+	public boolean step() throws LifeCycleException {
+		int hasard = rand.nextInt(angle_step + angle_step + 1) - angle_step;
+		int newAngle = outer.getAngle() + hasard;
+		
+		if(arls==null)
+			throw new LifeCycleException("AgentRefListService not bound");
+		AgentRef[] arefs = arls.getAgentRefs();
+		if(arefs==null) {
+			action = null; // cannot take any decision
+			System.out.println("FuirAgent failed for "+outer.getID());
+			return false; // step not finished
+		}
+		
+		action = new Action(Action.MOVE,newAngle);
+		return true; // step is finished
+/*
+ * Pour cela, la méthode step() doit e􏰋ffectuer un déplacement élémentaire
+ *  selon un angle calculé à partir de l'angle courant 
+ *  (outer.getAngle()) auquel on ajoute ou on retranche une valeur prise 􏰇au hasard􏰈 entre -angle_step et +angle_step, 
+ * la variable angle_step étant un paramètre du comportement.
+ * 
+ * */
+	}
+}
